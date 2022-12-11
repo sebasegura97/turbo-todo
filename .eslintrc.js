e06df73d51a9e5b222ca90d134512e43d8d335a8
sebasegura97 <sebasegura97@gmail.com>
2022-12-10 22:10:49 -0300

    fix(chore): fix lint with js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+// Why this error: https://github.com/vercel/next.js/issues/40687
+
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
