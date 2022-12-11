ecbbe2c31c883a4568463a4c7e043e74c6b92731
sebasegura97 <sebasegura97@gmail.com>
2022-12-11 15:09:52 -0300

    feat(chore): complete stack config with firebase tailwind and react-query

--- apps/web/utils/firebase.ts
+++ apps/web/utils/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAB__Inv29cJ5ylISYQY4LHLIeoFxZLr7s",
-  authDomain: "todo-app-cd552.firebaseapp.com",
-  projectId: "todo-app-cd552",
-  storageBucket: "todo-app-cd552.appspot.com",
-  messagingSenderId: "5980170796",
-  appId: "1:5980170796:web:eee5e66f754f42cb007f89",
-  measurementId: "G-XS0E67K61C",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- apps/web/views/HomeUser/index.tsx
+++ apps/web/views/HomeUser/index.tsx
@@ -1,6 +1,16 @@
 import { Button } from "ui";
+import { firestore } from "../../utils/firebase";
+
+import { useFirestoreQueryData } from "@react-query-firebase/firestore";
+import { collection, query } from "firebase/firestore";
 
 export default function Homepage() {
+  const ref = query(collection(firestore, "todo-lists"));
+
+  const data = useFirestoreQueryData(["todo-lists"], ref);
+
+  console.log("todo", data.data);
+
   return (
     <div>
       <h1>Hola mundo</h1>
