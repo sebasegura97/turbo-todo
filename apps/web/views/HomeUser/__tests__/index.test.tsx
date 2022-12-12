b1f07401c1fb864a005f9bd50c90bd6a391d6764
sebasegura97 <sebasegura97@gmail.com>
2022-12-12 20:17:24 -0300

    feat(chore): add needed configuration

--- apps/web/views/HomeUser/__tests__/index.test.tsx
+++ apps/web/views/HomeUser/__tests__/index.test.tsx
@@ -1,20 +1,6 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import Home from "../index";
 
 test("use jsdom in this test file", () => {
   const element = document.createElement("div");
   expect(element).not.toBeNull();
-});
-
-describe("Home", () => {
-  it("renders a h1", () => {
-    render(<Home />);
-
-    const heading = screen.getByRole("heading", {
-      name: "Hola mundo",
-    });
-
-    expect(heading).toBeDefined();
-  });
 });
